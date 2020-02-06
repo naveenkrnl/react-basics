dd34271d5c624f4f2f3da9a1d67c196d91d1d2eb
naveenkrnl <naveenarorakrnl@gmail.com>
2020-02-06 16:06:51 +0530

    Separating components and applying css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from "./components/header"
+import MainContent from "./components/maincontent"
+import Footer from "./components/footer"
+
+function App() {
+    return (<div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>)
+}
+
+export default App;
